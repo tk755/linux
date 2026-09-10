7e0bb51800d9e429c09d2b46fea86df686030824
Tushar Khan <dev@tusharkhan.com>
2026-09-09 23:06:14 -0400

    share gtk settings import and simplify niri startup

--- .config/niri/scripts/wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wallpaper_dir="$HOME/.wallpapers"
-if [[ -f "$wallpaper_dir/$HOSTNAME" ]]; then
-    exec swaybg -i "$wallpaper_dir/$HOSTNAME" -m fill
-elif [[ -f "$wallpaper_dir/default" ]]; then
-    exec swaybg -i "$wallpaper_dir/default" -m fill
-fi
